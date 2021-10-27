1af6bf423dde51185e0ec15466d933f5237629cd
AdapaRamakrishna <93036248+AdapaRamakrishna@users.noreply.github.com>
2021-10-27 16:27:16 +0530

    Add files via upload

---
+++ sum of first n even  and odd natural numbers.py
@@ -0,0 +1,9 @@
+#SUM OF FIRST N EVEN NATURAL NUMBERS
+m=int(input('Enter a number: '))
+r=m*(m+1)
+print("SUM OF FIRST", m, "EVEN NATURAL NUMBERS IS:", r)
+
+#SUM OF FIRST N ODD NATURAL NUMBERS
+n=int(input('Enter a number: '))
+s=n**2
+print("SUM OF FIRST", n, "ODD NATURAL NUMBERS IS:", s)
